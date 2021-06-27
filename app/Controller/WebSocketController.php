4eb83875f762b4c9de6fafcf4b979048d23ed6d4
Pader <ypnow@163.com>
2021-06-27 22:37:53 +0800

    Update websocket comment and some.

--- app/Controller/WebSocketController.php
+++ app/Controller/WebSocketController.php
@@ -2,26 +2,31 @@
 
 namespace App\Controller;
 
-use Wind\Process\ProcessStat;
 use Wind\Web\WebSocketInterface;
-use Workerman\Timer;
-
-use function Amp\asyncCoroutine;
 use function Amp\delay;
 
 class WebSocketController implements WebSocketInterface
 {
 
+    /**
+     * @inheritDoc
+     */
     public function onConnect($connection, $vars) {
         echo "Connection {$connection->id} connected.\n";
         $connection->send('Welcome to WebSocketServer.');
     }
 
+    /**
+     * @inheritDoc
+     */
     public function onMessage($connection, $data) {
         yield delay(1000);
         $connection->send('You have sent: '.$data);
     }
 
+    /**
+     * @inheritDoc
+     */
     public function onClose($connection) {
         echo "Connection {$connection->id} closed.\n";
     }

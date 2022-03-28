22ddeb06a70a5b0103000868ac7eed32ae91bef2
杨培 <ypnow@163.com>
2022-03-28 17:25:37 +0800

    Websocket server callbacks configure example.

--- config/websocket.php
+++ config/websocket.php
@@ -1,6 +1,10 @@
 <?php
 
 return [
+    'callbacks' => [
+        'on_worker_start' => '\App\Controller\WebSocketController::onWorkerStart',
+        'on_worker_stop' => '\App\Controller\WebSocketController::onWorkerStop'
+    ],
     'routes' => [
         '/chat' => \App\Controller\WebSocketController::class
     ]

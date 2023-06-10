016bdc68eb9cd28f25b11a809f4b16afb5040bb9
Pader <ypnow@163.com>
2023-06-10 15:19:02 +0800

    Async log demo update.

--- config/log.php
+++ config/log.php
@@ -1,7 +1,8 @@
 <?php
 
 use Monolog\Formatter\LineFormatter;
-use Monolog\Logger;
+use Monolog\Level;
+use Wind\Log\LogFactory;
 
 return [
     'default' => [
@@ -11,14 +12,49 @@ return [
                 'args' => [
                     'filename' => RUNTIME_DIR.'/log/app.log',
                     'maxFiles' => 15,
-                    'level' => Logger::INFO
+                    'level' => Level::Info
                 ],
                 'async' => true //启用 TaskWorker 模式异步写入
             ],
             [
                 'class' => \Wind\Log\StdoutHandler::class,
                 'args' => [
-                    'level' => Logger::INFO
+                    'level' => Level::Info
+                ],
+                //为 handler 设置独立的 formatter
+                'formatter' => [
+                    'class' => LineFormatter::class,
+                    'args' => [
+                        'dateFormat' => 'Y-m-d H:i:s',
+                        'allowInlineLineBreaks' => true
+                    ]
+                ]
+            ]
+        ],
+        //整个组默认的 formatter
+        'formatter' => [
+            'class' => LineFormatter::class,
+            'args' => [
+                'dateFormat' => 'Y-m-d H:i:s v',
+                'allowInlineLineBreaks' => true
+            ]
+        ]
+    ],
+    'task' => [
+        'handlers' => [
+            [
+                'class' => \Monolog\Handler\RotatingFileHandler::class,
+                'args' => [
+                    'filename' => RUNTIME_DIR.'/log/task.log',
+                    'maxFiles' => 15,
+                    'level' => Level::Info
+                ],
+                'async' => LogFactory::ASYNC_LOG_WRITER //启用 LogWriter 模式异步写入
+            ],
+            [
+                'class' => \Wind\Log\StdoutHandler::class,
+                'args' => [
+                    'level' => Level::Info
                 ],
                 //为 handler 设置独立的 formatter
                 'formatter' => [
@@ -39,4 +75,5 @@ return [
             ]
         ]
     ]
+
 ];
--- config/process.php
+++ config/process.php
@@ -1,8 +1,9 @@
 <?php
 
 return [
-    App\Process\MyProcess::class,
+    Wind\Log\LogWriterProcess::class,
     Wind\Crontab\CrontabDispatcherProcess::class,
+    App\Process\MyProcess::class,
     Wind\Queue\ConsumerProcess::class,
     // App\Process\MyConsumer::class,
 ];

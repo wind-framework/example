7f388b4dff160cd6b5d070e296acea9bf7e287c4
Pader <ypnow@163.com>
2023-09-10 17:05:55 +0800

    Annotation and console examples update.

--- config/config.php
+++ config/config.php
@@ -5,8 +5,9 @@ return [
     'debug' => true,
     'max_stack_trace' => 50,
     'annotation' => [
-        'scan' => [
-            'App\\Controller' => BASE_DIR.'/app/Controller'
+        'scan_ns_paths' => [
+            'App\\Controller@server' => BASE_DIR.'/app/Controller',
+            'App\\Command@console' => BASE_DIR.'/app/Command'
         ]
     ]
 ];
---
+++ config/console.php
@@ -0,0 +1,9 @@
+<?php
+//Global Config
+return [
+    'command' => [
+        'scan_ns_paths' => [
+            'App\\Command' => BASE_DIR.'/app/Command'
+        ]
+    ]
+];
